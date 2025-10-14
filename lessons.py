6748f5deca8b3ed19d2eca2ee6ff1b688cb0cf4c
Tokhir2311 <toxirjuraboyev23@gmail.com>
2025-10-14 15:51:13 +0000

    wrapper thme

--- lessons.py
+++ lessons.py
@@ -25,14 +25,13 @@ print(bool(15))
 print(10%3, 10/3, 10//7)
 x: int = 10
 print(x<5 and x<10)
-print(x<5 or x<4)'''
+print(x<5 or x<4)
 #print(bool("'"))
 
 x = int(input("x "))
 #if x > 10 : print("Eshmat") else :  print("Toshmat")
 print("eshmat") if x>10 else print("toshmat")
 
-'''
 
 person: dict = {
   name : "John",
@@ -73,7 +72,7 @@ def pos_num(son:int) ->bool:
             poss.append(i)
     return poss
 res = pos_num((1, 2, -3, 5, -9))
-print(res)'''
+print(res)
             
 def ds_pet(name:str, **details)->None:
     return name, details
@@ -82,3 +81,98 @@ print(res)
 
 
 print("hello")
+
+
+
+from typing import Callable 
+
+def funk1(f: Callable):
+    def ichki(*arg, **kwarg):
+        print("starting...")
+        f()
+        print('done')
+    return ichki
+    
+    
+@loggger.funk1   
+def funk2(*arg, **kwarg):
+    print(arg, kwarg)
+
+        
+funk2(5, a=12) 
+
+
+def decorator(funk):
+    def wrapper(*arg, **kwargs):
+        ret = funk(*arg, **kwargs)
+        print(ret.upper())
+    return wrapper
+    
+@decorator                                              o`xshadi 
+def say(s:str):
+    return "Salom "+s
+    
+say("Xayot")
+
+
+
+
+def decorator(funk):
+    def wrapper(s: str):
+        if s == "":
+            print("Not found")
+            return
+        else :
+            print(s)
+            return
+        funk(s)
+    return wrapper      
+@decorator         
+def string(s):
+    print(s+ " here is my boy")
+    
+string("h")
+
+
+
+
+def decorator(funk):
+    def negative(son: int):
+        if son<0:
+            
+            print(f"{son} is negative")
+            return
+        else:
+            print(f"{son} is positive")
+            return
+        negative(son)
+        son_ol(son)
+    return negative
+
+@decorator 
+def son_ol(son):
+    print(son**2)
+
+son_ol(5)
+
+
+
+
+
+
+
+def decor(funk):
+    def wrapper(*args, **kwargs):
+        l = args[0]
+        ls = sorted(l)
+        funk(ls)
+    return wrapper
+        
+@decor
+def funksiya(*args, **kwargs):
+    print(args)
+
+funksiya((1, 2, 4, 3, 7, 4))
+'''
+
+
